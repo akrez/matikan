2683ec4d2ce056d8f9bfb2676efc514c9b912c66
akrez <akrez.like@gmail.com>
2019-05-21 01:49:44 +0430

    hfgk

---
+++ modules/v1/controllers/PostController.php
@@ -0,0 +1,127 @@
+<?php
+namespace app\modules\v1\controllers;
+
+use app\controllers\Controller;
+use app\models\Status;
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\data\Pagination;
+use yii\data\Sort;
+use yii\helpers\ArrayHelper;
+use yii\web\NotFoundHttpException;
+use yii\web\UploadedFile;
+
+class PostController extends Controller
+{
+
+    public $modelClass = 'app\modules\v1\models\Post';
+
+    public function behaviors()
+    {
+        $behaviors = [
+            'authenticator' => [],
+            'access' => [
+                'rules' => [
+                    [
+                        'actions' => ['index'],
+                        'allow' => true,
+                        'verbs' => ['POST', 'GET', 'DELETE'],
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+        return array_merge_recursive($behaviors, parent::behaviors());
+    }
+
+    public function actionIndex($id = null)
+    {
+        $method = Yii::$app->request->getMethod();
+        if ($id) {
+            if ($method == 'GET' || $method == 'POST') {
+                return $this->update($id);
+            } elseif ($method == 'DELETE') {
+                return $this->delete($id);
+            }
+        } else {
+            if ($method == 'GET') {
+                return $this->index();
+            }
+        }
+        throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
+    }
+
+    public function index()
+    {
+        $modelClass = (new $this->modelClass);
+        $query = $modelClass::find()->where(['AND', ['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]], ['user_id' => Yii::$app->user->getId()]]);
+
+        $sortAttributes = [
+            'created_at' => ['label' => \Yii::t('app', 'Created At')],
+            'title' => ['label' => \Yii::t('app', 'Title')],
+        ];
+
+        $sort = new Sort([
+            'defaultOrder' => ['created_at' => SORT_DESC],
+            'attributes' => $sortAttributes,
+        ]);
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 20,
+            'pageSizeParam' => 'page_size'
+        ]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => $sort,
+            'pagination' => $pagination,
+        ]);
+
+        $models = $dataProvider->getModels();
+
+        $sortAttributeOrders = $dataProvider->getSort()->getAttributeOrders();
+        $sortAttribute = key($sortAttributeOrders);
+        $sortDirection = (reset($sortAttributeOrders) === SORT_ASC ? 'asc' : 'desc');
+
+        return [
+            'Posts' => ArrayHelper::toArray($models),
+            'Sort' => [
+                'direction' => $sortDirection,
+                'attribute' => $sortAttribute,
+                'attributes' => $sortAttributes,
+            ],
+            'Pagination' => [
+                'page_count' => $dataProvider->getPagination()->getPageCount(),
+                'page_size' => $dataProvider->getPagination()->getPageSize(),
+                'page' => $dataProvider->getPagination()->getPage(),
+                'total_count' => $dataProvider->getPagination()->totalCount,
+            ],
+        ];
+    }
+
+    public function update($id)
+    {
+        $model = $this->findOne($id);
+        if ($model->load(Yii::$app->request->post())) {
+            $model->image = UploadedFile::getInstance($model, 'image');
+            $model->save();
+        }
+        return $model->info();
+    }
+
+    public function delete($id)
+    {
+        $model = $this->findOne($id);
+        return ['status' => $model->delete()];
+    }
+
+    public function findOne($id)
+    {
+        $modelClass = (new $this->modelClass);
+        $model = $modelClass::find()->where(['AND', ['status' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]], ['user_id' => Yii::$app->user->getId()]])->one();
+        if (!$model) {
+            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
+        }
+        return $model;
+    }
+}
---
+++ modules/v1/models/Post.php
@@ -0,0 +1,115 @@
+<?php
+namespace app\modules\v1\models;
+
+use app\components\Helper;
+use app\components\jdf;
+use app\models\ActiveRecord;
+use app\models\Model;
+use app\models\Province;
+use app\models\Status;
+use app\models\User;
+use Yii;
+use yii\httpclient\Exception;
+use yii\imagine\Image;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $updated_at
+ * @property string $created_at
+ * @property string $status
+ * @property string $title
+ * @property string $isbn
+ * @property int $price
+ * @property int $province
+ * @property string $publishers
+ * @property string $writers
+ * @property string $translators
+ * @property int $publisher_year
+ * @property int $part
+ * @property string $cover
+ * @property int $user_id
+ *
+ * @property User $user
+ */
+class Post extends ActiveRecord
+{
+
+    public $image;
+
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    public function rules()
+    {
+        return [
+            [['status', 'title', 'isbn', 'price', 'province', '!user_id'], 'required'],
+            [['price'], 'integer', 'min' => -1],
+            [['part'], 'integer', 'min' => 1],
+            [['publisher_year'], 'integer', 'min' => 1357, 'max' => jdf::jdate('Y')],
+            [['status'], 'in', 'range' => [Status::STATUS_UNVERIFIED, Status::STATUS_ACTIVE, Status::STATUS_DISABLE]],
+            [['title', 'publishers', 'writers', 'translators'], 'string', 'max' => 512],
+            [['isbn'], 'match', 'pattern' => "/^[0-9]{1,1}[0-9x\-]{8,11}[0-9]{1,1}$/u"],
+            [['province'], 'in', 'range' => array_keys(Province::getList())],
+            [['image'], 'file', 'extensions' => ['jpg', 'png'], 'maxSize' => 1024 * 1024, 'mimeTypes' => ['image/jpeg', 'image/png']],
+            [['image'], 'uploadCoverValidation', 'skipOnError' => true],
+        ];
+    }
+
+    public function beforeValidate()
+    {
+        if (!parent::beforeValidate()) {
+            return false;
+        }
+        $this->publishers = Helper::normalizeArray($this->publishers);
+        $this->writers = Helper::normalizeArray($this->writers);
+        $this->translators = Helper::normalizeArray($this->translators);
+        $this->user_id = Yii::$app->user->getId();
+        return true;
+    }
+
+    public function uploadCoverValidation($attribute, $params, $validator)
+    {
+        try {
+            $imageSize = getimagesize($this->image->tempName);
+            if ($imageSize[0] > $imageSize[1] && (3 < $imageSize[0] / $imageSize[1])) {
+                return $this->addError($attribute, 'نسبت عرض به طول عکس نباید بیشتر از 3 برابر باشد.');
+            } elseif ($imageSize[1] > $imageSize[0] && (3 < $imageSize[1] / $imageSize[0])) {
+                return $this->addError($attribute, 'نسبت طول به عرض عکس نباید بیشتر از 3 برابر باشد.');
+            }
+
+            $ext = explode('/', $this->image->type);
+            $desFile = Yii::getAlias('@webroot') . DIRECTORY_SEPARATOR . 'gallery' . DIRECTORY_SEPARATOR . 'cover';
+            do {
+                $name = substr(uniqid(rand(), true), 0, 11) . '.' . $ext[1];
+            } while (file_exists($desFile . DIRECTORY_SEPARATOR . $name));
+            $desFile = $desFile . DIRECTORY_SEPARATOR . $name;
+            $image = Image::getImagine()->open($this->image->tempName);
+            Image::resize($image, 300, 300, false, true)->save($desFile, ['quality' => 100]);
+            $this->cover = $name;
+        } catch (Exception $ex) {
+            $this->cover = null;
+        }
+    }
+
+    public function info(array $fields = [], array $expand = [], $recursive = true)
+    {
+        $errors = $this->errors;
+
+        return [
+            $this->formName() => [
+                'status' => empty($errors),
+                'attributes' => parent::toArray($fields, $expand, $recursive),
+                'errors' => $errors,
+            ],
+        ];
+    }
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
